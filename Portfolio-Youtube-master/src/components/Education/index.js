bc0898be9fa6e075c58415d4a4ad5a1204f65c99
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-10-25 22:08:45 +0530

    index

--- Portfolio-Youtube-master/src/components/Education/index.js
+++ Portfolio-Youtube-master/src/components/Education/index.js
@@ -1,14 +1,14 @@
 
 import React from 'react'
 import styled from 'styled-components'
-// import Timeline from '@mui/lab/Timeline';
-// import TimelineItem from '@mui/lab/TimelineItem';
-// import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
 // import TimelineConnector from '@mui/lab/TimelineConnector';
-// import TimelineContent from '@mui/lab/TimelineContent';
-// import TimelineDot from '@mui/lab/TimelineDot';
-// import { education, experiences } from '../../data/constants';
-// import EducationCard from '../Cards/EducationCard';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { education } from '../../data/constants';
+import EducationCard from '../Cards/EducationCard';
 
 const Container = styled.div`
     display: flex;
@@ -86,7 +86,7 @@ const index = () => {
                     My education has been a journey of self-discovery and growth. My educational details are as follows.
                 </Desc>
                 <TimelineSection>
-                    {/* <Timeline>
+                    <Timeline>
                         {education.map((education,index) => (
                             <TimelineItem >
                                 <TimelineContent sx={{ py: '12px', px: 2 }}>
@@ -94,11 +94,11 @@ const index = () => {
                                 </TimelineContent>
                                 <TimelineSeparator>
                                     <TimelineDot variant="outlined" color="secondary" />
-                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                    {/* {index !== experiences.length  && <TimelineConnector style={{ background: '#854CE6' }} />} */}
                                 </TimelineSeparator>
                             </TimelineItem>
                         ))}
-                    </Timeline> */}
+                    </Timeline>
 
                 </TimelineSection>
             </Wrapper>
@@ -106,4 +106,4 @@ const index = () => {
     )
 }
 
-export default index
+export default index;
